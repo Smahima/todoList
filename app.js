02cb511c0dea289db6745bf7bca30bbd00dcbd78
Your name <Mahima Subramanian>
2017-08-17 12:57:35 -0500

    set up mustache and mark complete button

--- app.js
+++ app.js
@@ -2,14 +2,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mustacheExpress = require('mustache-express');
 const expressValidator = require('express-validator');
-let tasks = [{
+const tasks = [{
       'text': "Laundry",
       'done': true,
       'id':1
     },
+    {
+          'text': "Clean apt",
+          'done': false,
+          'id':2
+        }
   ]
 
-let app = express();
+const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(expressValidator())
@@ -19,12 +24,20 @@ app.set('views','./views');
 app.set('view engine', 'mustache')
 
 app.get("/", function (req, res) {
-  res.render('index.mustache');
-});
+  res.render('index.mustache', {tasks: tasks});
+})
 
 app.post("/", function (req, res) {
-  tasks.push(req.body.tasks);
-  res.redirect('/');
+
+  let max = tasks.length
+  const newestTask = req.body.newTask
+  let list = {
+    'text': newestTask,
+    'done': false,
+    'id': max + 1
+    }
+    tasks.push(list);
+    res.redirect('/');
 })
 
 app.listen(3000, function(){
