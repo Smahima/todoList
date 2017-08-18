c1e56650bac65e8a518aa20a90fdbb8029ae2c2e
Your name <Mahima Subramanian>
2017-08-17 19:57:36 -0500

    just need to pass items to completed section after clicking button. Last task

--- app.js
+++ app.js
@@ -3,15 +3,30 @@ const bodyParser = require('body-parser');
 const mustacheExpress = require('mustache-express');
 const expressValidator = require('express-validator');
 const tasks = [{
-      'text': "Laundry",
-      'done': true,
-      'id':1
-    },
-    {
-          'text': "Clean apt",
-          'done': false,
-          'id':2
-        }
+        'text': "Laundry",
+        'done': true,
+        'id':1
+        },
+      {
+        'text': "Clean apt",
+        'done': false,
+        'id':2
+      },
+        {
+        'text': "Make money",
+        'done': false,
+        'id':3
+      },
+        {
+        'text': "Meet Matt Damon",
+        'done': false,
+        'id':4
+        },
+      {
+        'text': "Marry Matt Damon",
+        'done': false,
+        'id':5
+      },
   ]
 
 const app = express();
@@ -29,7 +44,7 @@ app.get("/", function (req, res) {
 
 app.post("/", function (req, res) {
 
-  let max = tasks.length
+  let maxId = tasks.length
   const newestTask = req.body.newTask
   let list = {
     'text': newestTask,
@@ -38,6 +53,17 @@ app.post("/", function (req, res) {
     }
     tasks.push(list);
     res.redirect('/');
+})
+
+app.post('/:id', function (res, req) {
+    let id = parseInt(req.param.id);
+
+    tasks.forEach(function moveToComplete() {
+      if (id === moveToComplete.id) {
+        moveToComplete.id = true;
+      }
+    })
+    res.render('index.mustache', myData)
 })
 
 app.listen(3000, function(){
